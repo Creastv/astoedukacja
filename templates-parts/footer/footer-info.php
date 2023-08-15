d93315c01c59ab5fc1affd0eaa1f2493021fa286
Creastv <piotr@roial.pl>
2023-08-15 12:03:15 +0200

    img

--- templates-parts/footer/footer-info.php
+++ templates-parts/footer/footer-info.php
@@ -2,6 +2,6 @@
     <hr>
     <div class="footer-info__wrap">
       
-        <p> <small>2013 - 2023 Fundacja Edukacji Astronomicznej  |  Realizacja <a href="https://managerka.pl" target="_blank" >Managerka.com</a> z udziałem <a href="https://creastv.pl" target="_blank" >Creastv.pl </a> | </small> <img decoding="async" loading="lazy" width="70" height="24" src="<?php echo get_template_directory_uri() ?>/src/img/cc.png" alt="CC BY-SA-NC 4.0 " ></p>
+        <p> <small>2013 - 2023 Fundacja Edukacji Astronomicznej  </small><small> |  Realizacja <a href="https://managerka.pl" target="_blank" >Managerka.com</a> z udziałem <a href="https://creastv.pl" target="_blank" >Creastv.pl </a></small> |  <img decoding="async" loading="lazy" width="70" height="24" src="<?php echo get_template_directory_uri() ?>/src/img/cc.png" alt="CC BY-SA-NC 4.0 " ></p>
     </div>
 </div>
